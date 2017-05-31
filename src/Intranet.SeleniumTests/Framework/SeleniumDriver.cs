84077d04210e1241f56ba6023c53889186051ddc
Victor Dalqvist <v.dalqvist@gmail.com>
2017-05-31 08:51:05 +0200

    chore: add framework scaffolding

---
+++ src/Intranet.SeleniumTests/Framework/SeleniumDriver.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intranet.SeleniumTests.Framework
+{
+    public class SeleniumDriver
+    {
+        public static IWebDriver driver;
+    }
+}
