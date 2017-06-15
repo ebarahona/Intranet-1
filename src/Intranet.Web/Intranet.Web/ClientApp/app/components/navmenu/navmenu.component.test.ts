cac8c480d5ff99f2072251ad5f3815d490881b4b
Victor Dalqvist <v.dalqvist@gmail.com>
2017-06-15 13:36:21 +0200

    chore: add first unit tests for navmenu

---
+++ src/Intranet.Web/Intranet.Web/ClientApp/app/components/navmenu/navmenu.component.test.ts
@@ -0,0 +1,40 @@
+﻿import { NavMenuComponent } from './navmenu.component'
+import { TestBed, ComponentFixture } from '@angular/core/testing'
+import { RouterLinkStubDirective, RouterLinkActiveStubDiretive } from '../../../../Tests/routerStub'
+
+let fixture: ComponentFixture<NavMenuComponent>
+let component: NavMenuComponent
+
+describe('NavMenu component', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                NavMenuComponent,
+                RouterLinkStubDirective,
+                RouterLinkActiveStubDiretive
+            ]
+        })
+            .compileComponents()
+            .then(() => {
+                fixture = TestBed.createComponent(NavMenuComponent)
+                component = fixture.componentInstance
+                fixture.detectChanges()
+            })
+    })
+
+    it('should have an "Is In" variable that starts as "false"', () => {
+        const IsIn = component.isIn
+        expect(IsIn).toBe(false)
+    })
+
+    it('should toggle the value of IsIn when toggleState() is called', () => {
+        const startValue = component.isIn
+        component.toggleState()
+        const toggledValue_1 = component.isIn
+        expect(toggledValue_1).not.toBe(startValue)
+
+        component.toggleState()
+        const toggledValue_2 = component.isIn
+        expect(toggledValue_2).toBe(startValue)
+    })
+})
