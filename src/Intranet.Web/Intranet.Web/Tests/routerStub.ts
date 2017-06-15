b888929483254fed6556449d8985f010da2c968e
Victor Dalqvist <v.dalqvist@gmail.com>
2017-06-15 13:32:51 +0200

    feat: add mock for routerLink and routerLinkActive

---
+++ src/Intranet.Web/Intranet.Web/Tests/routerStub.ts
@@ -0,0 +1,25 @@
+﻿import { Directive, Input } from '@angular/core';
+
+// RouterLink
+@Directive({
+    selector: '[routerLink]',
+    host: {
+        '(click)': 'onClick()'
+    }
+})
+export class RouterLinkStubDirective {
+    @Input('routerLink') linkParams: any;
+    navigatedTo: any = null;
+
+    onClick() {
+        this.navigatedTo = this.linkParams;
+    }
+}
+
+// RouterLinkActive
+@Directive({
+    selector: '[routerLinkActive]',
+})
+export class RouterLinkActiveStubDiretive {
+    @Input('routerLinkActive') listParams: any;
+}
