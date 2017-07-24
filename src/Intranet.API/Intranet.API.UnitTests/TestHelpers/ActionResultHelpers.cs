24f8ec0ab8f39fed660f6c08e3ad4581ab0ba994
Oskar Klintrot <oskar.klintrot@gmail.com>
2017-07-24 13:29:30 +0200

    test(api): Adding tests for FAQ controller

--- src/Intranet.API/Intranet.API.UnitTests/TestHelpers/ActionResultHelpers.cs
+++ src/Intranet.API/Intranet.API.UnitTests/TestHelpers/ActionResultHelpers.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 
 namespace Intranet.API.UnitTests.TestHelpers
@@ -11,8 +10,21 @@ namespace Intranet.API.UnitTests.TestHelpers
     {
         public static int CountItems(this IActionResult actionResult)
         {
-            var ValuePropValue = actionResult.GetType().GetProperty("Value").GetValue(actionResult);
-            return (int)ValuePropValue.GetType().GetProperty("Count").GetValue(ValuePropValue);
+            return GetResponsesAs<object>(actionResult).Count();
+        }
+
+        public static IEnumerable<T> GetResponsesAs<T>(this IActionResult actionResult)
+            where T : class
+        {
+            var okObjectResult = actionResult as OkObjectResult;
+            return okObjectResult.Value as IEnumerable<T>;
+        }
+
+        public static T GetResponseAs<T>(this IActionResult actionResult)
+            where T : class
+        {
+            var okObjectResult = actionResult as OkObjectResult;
+            return okObjectResult.Value as T;
         }
     }
 }
