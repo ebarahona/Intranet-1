9faecdd67b2b10a41ee767a1f50f058c8c6f0c54
Oskar Klintrot <oskar.klintrot@gmail.com>
2017-07-27 14:37:10 +0200

    feat(web): Function for grouping FAQ's by category

--- src/Intranet.Web/Intranet.Web/ClientApp/app/_services/faq.service.ts
+++ src/Intranet.Web/Intranet.Web/ClientApp/app/_services/faq.service.ts
@@ -1,5 +1,6 @@
 ﻿import { Injectable } from '@angular/core'
 import { Response, Headers, RequestOptions } from '@angular/http'
+import * as _ from 'lodash'
 
 // Grab everything with import 'rxjs/Rx'
 import { Observable } from 'rxjs/Observable'
@@ -12,7 +13,7 @@ import 'rxjs/add/operator/toPromise'
 import { SecureHttpService } from './'
 import { RestService } from './rest.service'
 
-import { Faq } from '../models'
+import { Faq, FaqByCategory, Category } from '../models'
 
 @Injectable()
 export class FaqService extends RestService<Faq> {
@@ -20,5 +21,29 @@ export class FaqService extends RestService<Faq> {
     http: SecureHttpService,
   ) {
     super(http, 'faq')
+  }
+
+  getFaqsByCategory(): Observable<FaqByCategory[]> {
+    return this.http.get(this.url)
+      .map((res: Response) => {
+        return res.json()
+      })
+      .map((faqs: Faq[]) => {
+        return (_
+          .chain(faqs)
+          .flatMap(faq => faq.category) as any) // Not pretty but it works: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/6586#issuecomment-292532489
+          .uniqWith((arrVal: Category, othVal: Category) => {
+            return arrVal.id === othVal.id
+          })
+          .map((category: Category) => {
+            const faqByCategory = new FaqByCategory()
+            faqByCategory.category = category
+            faqByCategory.faqs = _.filter(faqs, faq => faq.category.id === category.id)
+            return faqByCategory
+          })
+          .sortBy(fc => fc.category.title)
+          .value()
+      })
+      .catch(this.handleError)
   }
 }
